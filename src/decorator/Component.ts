b421ab665959a3b8fe95e498f476cd6058ec5b86
stLmpp <gui.stlmpp@hotmail.com>
2019-08-30 23:20:13 -0300

    0.7.2
    -Added an options to Component with Template injectable, just put a static method in your component class with the name of $templateResolver, and use the injections that you need

--- src/decorator/Component.ts
+++ src/decorator/Component.ts
@@ -15,6 +15,9 @@ export function Component(config: ComponentConfig = {}){
       transclude: config.transclude,
       templateUrl: config.templateUrl
     }
+    if (!component.template && !component.templateUrl && target.$templateResolver){
+      component.template = target.$templateResolver;
+    }
     target.$stComponent = component;
     target.$stComponentName = replace(type, config.selector, 'selector') || replace(type, target.name, 'name');
     target.$stType = type;
